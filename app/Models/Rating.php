d1ff3733f52b982a41af6d41acddc6fbe4446489
TheBeast22 <beastthe588@gmail.com>
2023-12-22 23:47:06 +0200

    adding relations for customers and ratings table

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "hotel_id","customer_id","rate"
+    ];
+    protected $casts = [
+        "rate"=>"integer"
+    ];
+    public function customer(){
+        return $this->belongsTo(Customer::class);
+    }
+    public function hotel(){
+        return $this->belongsTo(Hotel::class);
+    }
 }
