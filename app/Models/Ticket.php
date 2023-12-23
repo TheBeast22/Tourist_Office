717b809c53941eb7b002de1cff5bf8c73bb16272
msmariam2078 <miss.mariam.essa1996@gmail.com>
2023-12-23 15:34:21 -0800

    update in ticket model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Company;
+use App\Models\Hotel;
+use App\Models\Customer;
+use App\Models\City;
 class Ticket extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'company_id',
+        'city_id',
+        'date_s',
+        'date_e'
+    ];
+    protected $casts = [
+        "company_id"=> "integer",
+        "city_id"=> "integer",
+        "date_s"=> "date",
+        "date_e"=> "date"
+    ];
+
+public function company(){
+
+ return $this->belongsTo(Company::class);
 }
+public function city(){
+    
+ return $this->belongsTo(City::class);
+}
+
+
+public function customers(){
+    return $this->belongsToMany(Customer::class,"bookings");
+}
+public function hotels(){
+    return $this->belongsToMany(Hotel::class,"bookings");
+}}
