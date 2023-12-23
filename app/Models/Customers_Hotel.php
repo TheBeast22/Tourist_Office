c98b6f6f854f6f0ff346c76dbf3c750cf26c8105
TheBeast22 <beastthe588@gmail.com>
2023-12-24 00:23:01 +0200

    adding customers_hotels table and modefied some important schema details

---
+++ app/Models/Customers_Hotel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customers_Hotel extends Model
+{
+    use HasFactory;
+}
