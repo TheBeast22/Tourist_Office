912946114c8198438dd79a6401c7601ed8e27213
abdallahhahh <ases3608@gmail.com>
2023-12-24 14:52:55 +0300

    branch

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Hotel extends Model
 {
     use HasFactory;
+    protected $fillable = ["name","city_id"];
+    protected $casts = [
+        "name"=> "string",
+        "city_id"=>"integer"
+    ];
+
+public function city(){
+    return $this->belongsTo(City::class);
+}
+
+
+
+    public function bookings(){
+    return $this->hasMany(Booking::class);
+    }
+
+
+    public function ratings(){
+        return $this->hasMany(Rating::class);
+    }
+     
+
+    public function customershotel(){
+        return $this->belongsToMany(customer::class);
+    }
 }
